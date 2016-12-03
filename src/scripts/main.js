4a849a5feae466faa05574842e6dcd5cef9850a4
Kirill Fadeev <fk@grapheme.ru>
2016-12-03 20:14:53 +0300

    some more gameplay

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,7 +14,6 @@ class GAME {
     this.canvas = document.createElement('canvas');
     this.context = this.canvas.getContext('2d');
     this.fps = 60;
-    this.bodies = [];
     this.invaders = 30;
     this.objects = [];
 
@@ -31,6 +30,7 @@ class GAME {
     ];
 
     this.objects = this.objects.concat(new Ship(this, this.canvasSize, this.images[1]));
+    this.objects = this.objects.concat(this.swarmUp(this.images[0]));
     document.body.appendChild(this.canvas);
     this.resizeCanvas();
     this.handleEvents();
@@ -38,7 +38,7 @@ class GAME {
     var gameLoop = () => {
       const t = this;
 
-      setTimeout(function() {
+      setTimeout(() => {
         t.update();
         t.render(t.context, t.canvasSize);
         window.requestAnimationFrame(gameLoop);
@@ -48,7 +48,26 @@ class GAME {
     this.preloadImages(gameLoop);
   }
 
+  checkCollisions(firstObject, secondObject) {
+    return !(
+      firstObject === secondObject ||
+      firstObject.position.posX + firstObject.size.width / 2 < secondObject.position.posX - secondObject.size.width / 2 ||
+      firstObject.position.posY + firstObject.size.height / 2 < secondObject.position.posY - secondObject.size.height / 2 ||
+      firstObject.position.posX - firstObject.size.width / 2 > secondObject.position.posX + secondObject.size.width / 2 ||
+      firstObject.position.posY - firstObject.size.height / 2 > secondObject.position.posY + secondObject.size.height / 2
+    );
+  };
+
   update() {
+    const survivers = (firstObject) => {
+      return (this.objects.filter(
+        (secondObject) => {
+          return (this.checkCollisions(firstObject, secondObject));
+        }
+      ).length === 0);
+    };
+
+    this.objects = this.objects.filter(survivers);
     for (let i = 0; i < this.objects.length; i++) {
       this.objects[i].update();
     }
@@ -74,6 +93,48 @@ class GAME {
       object.size.width,
       object.size.height
     );
+  }
+
+  toModule(number) {
+    if (number < 0) {
+      number = -number;
+    }
+
+    return number
+  }
+
+  lowerRow(being) {
+    return this.objects.filter((haveFriends) => {
+      return (
+        haveFriends instanceof Being &&
+        this.toModule(being.position.x - haveFriends.position.x) < haveFriends.size.width &&
+        haveFriends.position.posY > being.position.posY
+      );
+    }).length > 0;
+  }
+
+  swarmUp(imgSrc) {
+    let swarm = [];
+
+    for (let i = 0; i < 60; i++) {
+
+      const
+        posX = 10 + (i % 12) * 80,
+        posY = 10 + (i % 5) * 60;
+
+      // Create invader.
+      swarm.push(
+        new Being(
+          this,
+          {
+            posX: posX,
+            posY: posY
+          }
+        )
+      );
+    }
+
+    return swarm;
   }
 
   preloadImages(callback) {
@@ -148,7 +209,7 @@ class Ship {
       posY: Math.round(canvasSize.height - (this.size.height + 10))
     };
 
-    this.hanbleEvents();
+    this.handleEvents();
   }
 
   laserTime() {
@@ -181,7 +242,7 @@ class Ship {
 
   }
 
-  hanbleEvents() {
+  handleEvents() {
     const t = this;
 
     window.onkeydown = (e) => {
@@ -189,6 +250,45 @@ class Ship {
 
       t.playerAction(pressedKey);
     }
+  }
+}
+
+class Being {
+  constructor(game, position) {
+    this.game = game;
+    this.image = document.createElement('img');
+    this.image.src = this.game.images[0];
+    this.position = position;
+    this.directionX = 0;
+    this.speedX = 2;
+    this.speedY = 0;
+    this.size = {
+      width: 57,
+      height: 41
+    };
+  }
+
+  update() {
+    if (this.directionX < 0 || this.directionX > this.game.canvasSize.width - ((this.size.width + 24) * 12)) {
+      this.speedX = -this.speedX;
+    }
+
+    if (Math.random() > 0.9993 && !this.game.lowerRow(this)) {
+      const laser = new Laser(
+        {
+          posX: this.position.posX,
+          posY: this.position.posY + Math.round(this.size.width / 2)
+        }, {
+          x: Math.round(Math.random() - Math.random()),
+          y: 5
+        },
+        this.game.images[2]
+      );
+      this.game.objects.push(laser);
+    }
+
+    this.position.posX += this.speedX;
+    this.directionX += this.speedX;
   }
 }
 
